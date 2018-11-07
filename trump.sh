cd3ddf8522be919ad788bc896c8a43dcacd03bd2
Mitchell Moser <moser.mitch@yahoo.com>
2018-11-07 03:20:28 -0800

    Update trump.sh

--- trump.sh
+++ trump.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# check to see if script is running
+# check to see if trump.py is running
 var=$(pgrep -f trump.py)
-# if it is not, start it
+# if it is not, start it in the background
 if [[ ! "$var" ]]
 then
 	python3 /path/to/file/trump.py &
